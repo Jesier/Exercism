96313cffe4158ef7cd0f72246ce4c6d2d5f81828
Jesier <jesiersantiago@gmail.com>
2023-07-19 15:28:29 -0500

    Cooking lasanga function practice

---
+++ Problems/Lasagna.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Exercism.Problems
+{
+    class Lasagna
+    {
+        // TODO: define the 'ExpectedMinutesInOven()' method
+        public int ExpectedMinutesInOven() => 40;
+
+        // TODO: define the 'RemainingMinutesInOven()' method
+        public int RemainingMinutesInOven(int remainingminutes) => ExpectedMinutesInOven() - remainingminutes;
+        // TODO: define the 'PreparationTimeInMinutes()' method
+        public int PreparationTimeInMinutes(int layers) => layers * 2;
+        // TODO: define the 'ElapsedTimeInMinutes()' method
+        public int ElapsedTimeInMinutes(int numberoflayers, int numberofminutes) => PreparationTimeInMinutes(numberoflayers) + numberofminutes;
+    }
+}
